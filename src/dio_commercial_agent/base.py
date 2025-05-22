a6687d5360f1e621121b097aea7c86e8f1b11fe2
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-05-22 17:51:16 +0500

    22.05.2025 add db models

--- src/dio_commercial_agent/base.py
+++ src/dio_commercial_agent/base.py
@@ -1,4 +1,8 @@
+from typing import Optional
+
 from abc import ABC, abstractmethod
+
+from .schemas import BaseMessage
 
 
 class AIAgent(ABC):
@@ -6,5 +10,9 @@ class AIAgent(ABC):
     async def generate(self, chat_id: str, text: str) -> str: pass
 
 
-class MessagesRepository(ABC):
-    pass
+class MessageRepository(ABC):
+    @abstractmethod
+    async def bulk_create(self, messages: list[BaseMessage]) -> None: pass
+
+    @abstractmethod
+    async def read(self, chat_id: str) -> Optional[list[BaseMessage]]: pass
---
+++ src/dio_commercial_agent/infrastructure/database/repositories.py
@@ -0,0 +1,6 @@
+from src.dio_commercial_agent.base import MessageRepository
+
+
+class SQLMessageRepository(MessageRepository):
+    def __init__(self) -> None:
+        ...
